d9894d66071987b2191fc888be09809f683925dc
Ragu Sivanandha <ragruraam.sivanandha@walmart.com>
2024-05-01 17:00:26 -0400

    added initial code

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# sample-gql-app
-sample app for walmart candidates
+# 👤 Nextjs with GraphQL server and client
+
+GraphQL server and client Set up
+
+### Front-end
+* React js
+* [Next UI](https://nextui.org/)
+* @apollo/client
+* @react-oauth/google
+
+### Back-end
+* @apollo/server
+* @as-integrations/next
+* graphql
+* graphql-tag
+* [Dummyjson](dummyjson.com)
+
+### Installig
+```
+pnpm run install
+```
+
+### Env
+```
+NEXT_PUBLIC_URL_SERVER_GRAPHQL=http://localhost:3000/api/graphql
+URL_API=https://dummyjson.com/users
+```
+
+### Running app
+```
+pnpm run dev
+```
+
+<hr>
---
+++ src/graphql/resolvers.js
@@ -0,0 +1,46 @@
+
+// https://sample-walmart.free.beeceptor.com/
+const resolvers = {
+  Query: {
+    users: async () => {
+      try {
+        const response = await fetch(`${process.env.URL_API}`)
+        const data = await response.json()
+
+        return data.users.map(u => {
+          return {
+            id: u.id,
+            firstName: u.firstName,
+            lastName: u.lastName,
+            email: u.email,
+            username: u.username,
+            image: u.image
+          }
+        })
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+    searchUser: async (_, { value }) => {
+      try {
+        const response = await fetch(`${process.env.URL_API}/search?q=${value}`)
+        const data = await response.json()
+
+        return data.users.map(u => {
+          return {
+            id: u.id,
+            firstName: u.firstName,
+            lastName: u.lastName,
+            email: u.email,
+            username: u.username,
+            image: u.image
+          }
+        })
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+  }
+}
+
+export default resolvers
