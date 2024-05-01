1044719f3cda20054a4e23291368b15513e4677e
Ragu Sivanandha <ragruraam.sivanandha@walmart.com>
2024-05-01 17:06:45 -0400

    updated resolver

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -2,25 +2,7 @@
 // https://sample-walmart.free.beeceptor.com/
 const resolvers = {
   Query: {
-    users: async () => {
-      try {
-        const response = await fetch(`${process.env.URL_API}`)
-        const data = await response.json()
-
-        return data.users.map(u => {
-          return {
-            id: u.id,
-            firstName: u.firstName,
-            lastName: u.lastName,
-            email: u.email,
-            username: u.username,
-            image: u.image
-          }
-        })
-      } catch (error) {
-        throw new Error(error)
-      }
-    },
+    users: async () => {},
     searchUser: async (_, { value }) => {
       try {
         const response = await fetch(`${process.env.URL_API}/search?q=${value}`)
